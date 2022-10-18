a62aa2905ed736434f1ec8f17a3426a2795165d5
ivan2798 <inge1998@hotmail.com>
2022-10-18 17:29:17 -0500

    Excepciones V

--- bootcamp_CSharp_lesson_6/Program.cs
+++ bootcamp_CSharp_lesson_6/Program.cs
@@ -11,74 +11,39 @@ namespace bootcamp_CSharp_lesson_6
 
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Introduce n° de mes");
-
-            int numeroMes = int.Parse(Console.ReadLine());
+            System.IO.StreamReader archivo = null;
 
             try
             {
-                Console.WriteLine(NombreDelMes(numeroMes));
+                string linea;
+
+                int contador = 0;
+
+                string path = "D:\tirar.txt";
+
+                archivo = new System.IO.StreamReader(path);
+
+                while ((linea = archivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                    contador++;
+                }
 
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-
-                Console.WriteLine("Mensaje de la excepción: " + ex.Message);
+                Console.WriteLine("Error con la lectura del archivo");
+                
             }
 
-
-            Console.WriteLine("Aquí continuaría la ejecución del resto del programa");
+            finally 
+            {
+                if (archivo != null) archivo.Close();
+                Console.WriteLine("Archivo cerrado");
+            }
             
         }
 
-        public static string NombreDelMes(int mes)
-        {
-            switch (mes)
-            {
-                case 1:
-                    return "Enero";
-
-                case 2:
-                    return "Febrero";
-
-                case 3:
-                    return "Marzo";
-
-                case 4:
-                    return "Abril";
-
-                case 5:
-                    return "Mayo";
-
-                case 6:
-                    return "Junio";
-
-                case 7:
-                    return "Julio";
-
-                case 8:
-                    return "Agosto";
-
-                case 9:
-                    return "Septiembre";
-
-                case 10:
-                    return "Octubre";
-
-                case 11:
-                    return "Noviembre";
-
-                case 12:
-                    return "Diciembre";
-
-                default:
-
-                    //.NET framework library
-                    //lanzar
-                    throw new ArgumentOutOfRangeException();
-            }
-
-        }
+       
     }
 }
