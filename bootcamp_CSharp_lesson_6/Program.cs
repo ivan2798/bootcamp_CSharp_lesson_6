22c4629c52c48c3e06cd8a4287b4bf517c17156b
ivan2798 <inge1998@hotmail.com>
2022-10-18 17:18:28 -0500

    Excepciones IV

--- bootcamp_CSharp_lesson_6/Program.cs
+++ bootcamp_CSharp_lesson_6/Program.cs
@@ -11,17 +11,74 @@ namespace bootcamp_CSharp_lesson_6
 
         static void Main(string[] args)
         {
-            checked{
 
-                int numero = int.MaxValue;
+            Console.WriteLine("Introduce n° de mes");
 
-                int resultado = numero + 20;
+            int numeroMes = int.Parse(Console.ReadLine());
 
-                Console.WriteLine(resultado);
+            try
+            {
+                Console.WriteLine(NombreDelMes(numeroMes));
 
             }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Mensaje de la excepción: " + ex.Message);
+            }
+
+
+            Console.WriteLine("Aquí continuaría la ejecución del resto del programa");
+            
         }
 
+        public static string NombreDelMes(int mes)
+        {
+            switch (mes)
+            {
+                case 1:
+                    return "Enero";
 
+                case 2:
+                    return "Febrero";
+
+                case 3:
+                    return "Marzo";
+
+                case 4:
+                    return "Abril";
+
+                case 5:
+                    return "Mayo";
+
+                case 6:
+                    return "Junio";
+
+                case 7:
+                    return "Julio";
+
+                case 8:
+                    return "Agosto";
+
+                case 9:
+                    return "Septiembre";
+
+                case 10:
+                    return "Octubre";
+
+                case 11:
+                    return "Noviembre";
+
+                case 12:
+                    return "Diciembre";
+
+                default:
+
+                    //.NET framework library
+                    //lanzar
+                    throw new ArgumentOutOfRangeException();
+            }
+
+        }
     }
 }
